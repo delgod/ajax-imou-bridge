b9dba33fc5a30827c58515c3f4bdfcd3e3880608
Mykola Marzhan <mykola.marzhan@canonical.com>
2025-07-13 13:30:43 +0200

    log formating

--- sia_receiver.py
+++ sia_receiver.py
@@ -245,7 +245,7 @@ def configure_logging(log_level_str: str) -> None:
 
     logging.basicConfig(
         level=level,
-        format="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
